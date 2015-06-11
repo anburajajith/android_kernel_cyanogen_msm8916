e794c479ae017d4d2fb3db73b0f121fadb34cbc1
arter97 <qkrwngud825@gmail.com>
2015-06-11 22:01:45 +0900

    fix

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -39,6 +39,7 @@
 
 #define ADRENO_IDLER_MAJOR_VERSION 1
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define ADRENO_IDLER_MINOR_VERSION 1
 
 /* stats.busy_time threshold for determining if the given workload is idle.
@@ -68,6 +69,9 @@ module_param_named(adreno_idler_active, adreno_idler_active, bool, 0664);
 static unsigned int idlecount = 0;
 =======
 #define ADRENO_IDLER_MINOR_VERSION 0
+=======
+#define ADRENO_IDLER_MINOR_VERSION 1
+>>>>>>> ab498b3... adreno_idler: remove frequency bump
 
 /* stats.busy_time threshold for determining if the given workload is idle.
    Any workload higher than this will be treated as non-idle workload,
@@ -152,10 +156,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			return 1;
 		}
 	} else {
-		/* This is the case where msm-adreno-tz don't use the lowest frequency.
-		   Mimic this behavior by bumping up the frequency. */
 		idlecount = 0;
-		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 2];
 		/* Do not return 1 here and allow rest of the algorithm to
 		   figure out the appropriate frequency for current workload.
 		   It can even set it back to lowest frequency. */
