c860ca2c1777fd25daf255dcb95fa6825777b7a7
anburajajith <anburajajith@gmail.com>
2016-07-09 22:32:17 +0530

    Revert "kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)"
    This reverts commit b468a0afe17b3f6c7e4b1add5943c56ac7b4b154.

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -10,8 +10,6 @@
  *
  *  v1.2 - make kernel / userspace mode switchable
  *
- *  v1.3 - add a hook in display pannel driver as alternative kernel trigger
- *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -31,9 +29,8 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
-#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
-#define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
-#define POWER_SUSPEND_PANNEL	2	// Use display pannel state as hook
+#define POWER_SUSPEND_KERNEL	0
+#define POWER_SUSPEND_USERSPACE	1
 
 struct power_suspend {
 	struct list_head link;
@@ -44,6 +41,7 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
+<<<<<<< HEAD
 void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_pannel_hook(int new_state);
 <<<<<<< HEAD
@@ -51,6 +49,9 @@ void set_power_suspend_state_pannel_hook(int new_state);
 extern bool power_suspended;
 =======
 >>>>>>> parent of 37a823d... kernel/power/powersuspend: cumulative update to version 1.5
+=======
+void set_power_suspend_state_hook(int new_state);
+>>>>>>> parent of b468a0a... kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
 
 #endif
 
