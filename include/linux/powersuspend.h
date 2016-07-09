5fbb8fb4c982ed8657b33fc86f14fdee17e9b93d
anburajajith <anburajajith@gmail.com>
2016-07-09 22:33:57 +0530

    revert powersuspend

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -3,11 +3,6 @@
  * Copyright (C) 2007-2008 Google, Inc.
  * Copyright (C) 2013 Paul Reioux 
  *
- * Modified by Jean-Pierre Rasquin <yank555.lu@gmail.com>
- *
- *   make powersuspend not depend on a userspace initiator anymore,
- *   but use a hook in autosleep instead.
- *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -24,9 +19,6 @@
 
 #include <linux/list.h>
 
-#define POWER_SUSPEND_INACTIVE	0
-#define POWER_SUSPEND_ACTIVE	1
-
 struct power_suspend {
 	struct list_head link;
 	void (*suspend)(struct power_suspend *h);
@@ -36,6 +28,7 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_pannel_hook(int new_state);
@@ -48,5 +41,7 @@ extern bool power_suspended;
 void set_power_suspend_state_hook(int new_state);
 >>>>>>> parent of b468a0a... kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
 
+=======
+>>>>>>> parent of df26144... kernel/power/powersuspend: remove userspace dependency from powersuspend
 #endif
 
